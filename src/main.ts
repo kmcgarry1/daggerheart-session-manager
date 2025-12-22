11ec179f029369e7fa77c96f5bcc9aad3f95c6ec
kmcgarry1 <kevin.mc.garry.13@gmail.com>
2025-12-22 17:51:01 +0000

    added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
+import { inject } from "@vercel/analytics";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+
+if (import.meta.env.PROD) {
+  inject();
+}
 
 createApp(App).use(router).mount("#app");
